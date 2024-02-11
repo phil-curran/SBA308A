e253e0d77b00e0c299a1f05bc29cc81612c30122
Phil Curran <pecurran@outlook.com>
2024-02-11 11:49:27 -0800

    strip out firebase stuff, got fetch call working to api, pseudocode for javascript.

--- _js/main.js
+++ _js/main.js
@@ -1,2 +1,10 @@
-import "dotenv/config";
-console.log(process.env); // remove this
+// get weather api call
+const getWeather = async (count) => {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?id=524901&cnt=${count}&appid=c5a484c5c3125843e3a5fc1dbfe66e1b`
+  );
+  const jsonData = await response.json();
+  console.log(jsonData);
+};
+
+getWeather(7);
