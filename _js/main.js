f5a08222d6c5c9d781b55f061103dc314f7cb656
Phil Curran <pecurran@outlook.com>
2024-02-11 20:52:16 -0800

    maybe try supabase?

--- _js/main.js
+++ _js/main.js
@@ -1,3 +1,21 @@
-const api_key = "55d5ee9a1f8c314c5e3185fc0552d023";
+const api_key = "c5a484c5c3125843e3a5fc1dbfe66e1b";
 
-https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+http://api.openweathermap.org/geo/1.0/zip?zip=98011&appid=ed3a126ff6d2bf3265efc688d85505cb
+
+
+
+http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c5a484c5c3125843e3a5fc1dbfe66e1b
+
+
+http://api.openweathermap.org/data/2.5/forecast?id=524901&&cnt=7&appid=c5a484c5c3125843e3a5fc1dbfe66e1b
+
+
+
+https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=39.099724&lon=-94.578331&dt=1643803200&appid=c5a484c5c3125843e3a5fc1dbfe66e1b
+
+
+http://api.openweathermap.org/geo/1.0/zip?zip=98011,US&appid=c5a484c5c3125843e3a5fc1dbfe66e1b
+http://api.openweathermap.org/geo/1.0/direct?q=Seattle,WA,US&appid=ed3a126ff6d2bf3265efc688d85505cb
+
+http://api.openweathermap.org/geo/1.0/zip?zip=98011&appid=ed3a126ff6d2bf3265efc688d85505cb
